d018d6e114690ab7c8f7ccac3f2b562b1a045951
Grama Nicolae <gramanicu@gmail.com>
2022-05-22 15:39:36 +0300

    Solved small issue

--- src/components/complex/ServiceComponent.svelte
+++ src/components/complex/ServiceComponent.svelte
@@ -11,18 +11,16 @@
 {#if service && service.provider}
 	<div class="w-full max-w-sm p-4 border border-black rounded-md">
 		<div class="flex flex-row w-full items-center">
-			<h1 class="font-bold text-2xl mr-2">Service type:</h1>
+			<h1 class="font-bold text-2xl mr-2">{service.name}</h1>
 			<Badge text={parseServiceType(service.type)} class="text-2xl" />
 		</div>
 		<div class="py-2">
-			<a sveltekit:prefetch href="/dashboard/providers/{service.provider.id}">
-				<ImageLabel
-					img={User}
-					imgTheme="outline"
-					text="Provider: {service.provider.name}"
-					class="text-lg"
-				/>
-			</a>
+			<ImageLabel
+				img={User}
+				imgTheme="outline"
+				text="Provider: {service.provider.name}"
+				class="text-lg"
+			/>
 		</div>
 		{#if service.description != ''}
 			<div class="bg-gray-300 p-2 rounded-md mt-4 text-sm">
